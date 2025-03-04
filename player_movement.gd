3be45a81d9f8da7bf3cd5620da103bd2f3787171
matwa <mateowatata@gmail.com>
2025-03-04 14:31:56 -0500

    Added Dash Energy
    Agregarle una funciona  la recuperacion de energia al dash y hacer que
    se recupere dependiendo del tiempo.

--- player_movement.gd
+++ player_movement.gd
@@ -9,9 +9,18 @@ class_name Player
 @export_group("Dash")
 @export var DashCooldown: Timer 
 @export var DASH_SPEED =20
+@export var MAX_ENERGY = 100
+@export var ENERGY_PER_DASH = 10
+@export var energy: float = MAX_ENERGY
+
+
+@export_group("Test Stuff")
+@export var label: RichTextLabel
 var canDash: bool = true
 
 	
+
+
 func get_input() -> Vector2:
 	var x := Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	var y := Input.get_action_strength("Down") - Input.get_action_strength("Up")
@@ -32,14 +41,23 @@ func _physics_process(delta: float) -> void:
 	else:
 		playerInput.x = 0
 
-	print(playerInput)
+	
 	velocity = lerp(velocity, playerInput * SPEED, delta * ACCEL)
 	
-	if Input.is_action_just_pressed("Dash") and canDash:
+	if Input.is_action_just_pressed("Dash") and canDash and energy >= ENERGY_PER_DASH:
+		energy -= ENERGY_PER_DASH
 		canDash = false
 		DashCooldown.start()
 		dash()
 
+	if energy < MAX_ENERGY:
+		energy += (log(energy) / log(1.5) + 1) * delta 
+		energy = clamp(energy, 1, MAX_ENERGY)	
+
+
+	label.text = str(int(energy))
+
+	print(energy)
 	move_and_slide()
 
 
